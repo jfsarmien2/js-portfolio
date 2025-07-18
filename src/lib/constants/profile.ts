2ec85cee8de5220bf97b63d803787d1154ce1c73
John <john.sarmiento452@gmail.com>
2025-07-18 17:33:57 +0800

    fix spacing on profile info

--- src/lib/constants/profile.ts
+++ src/lib/constants/profile.ts
@@ -4,8 +4,7 @@ export const PROFILE_INFO = {
   slogan: 'Code, Learn, Evolve...',
   displayName: "John Sarmiento",
   email: "john.sarmiento452@gmail.com",
-  about: `
-Web developer focused on integrating RESTful and third-party APIs into modern front-end applications. Experienced at connecting design and development, with a passion for creating scalable and maintainable solutions that meet business needs. Proficient in modern JavaScript frameworks and tools. Committed to ongoing learning, teamwork, and solving real-world problems using technology.
+  about: `Web developer focused on integrating RESTful and third-party APIs into modern front-end applications. Experienced at connecting design and development, with a passion for creating scalable and maintainable solutions that meet business needs. Proficient in modern JavaScript frameworks and tools. Committed to ongoing learning, teamwork, and solving real-world problems using technology.
 `,
 } as const;
 
